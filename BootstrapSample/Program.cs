56a3e4b2d1dc264b0a6a89159b6244b72b7904fc
burakgunce <brk.gnc9@hotmail.com>
2023-12-19 10:31:58 +0300

    bootstrap sample added

---
+++ BootstrapSample/Program.cs
@@ -0,0 +1,20 @@
+namespace BootstrapSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("index.html");
+            app.UseDefaultFiles(options);
+            app.UseStaticFiles();
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
