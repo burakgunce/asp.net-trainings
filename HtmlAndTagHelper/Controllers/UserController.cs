2c6b745e5c3cd501c60b006da5a1967068ac05a3
burakgunce <brk.gnc9@hotmail.com>
2023-12-21 11:52:18 +0300

    tag helper added

---
+++ HtmlAndTagHelper/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using HtmlAndTagHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmlAndTagHelper.Controllers
+{
+    public class UserController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //https://localhost:Portnumber/User/HtmlHelpers
+        [HttpGet]
+        public IActionResult HtmlHelpers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult HtmlHelpers(User user)
+        {
+            return View();
+        }
+
+        public IActionResult HtmlHelpers2() => View();
+
+        public IActionResult TagHelpers() => View();
+
+        [HttpPost]
+        public IActionResult TagHelpers(User user)
+        {
+            User usernew = new User
+            {
+                UserName = "testuser",
+                Password = "123456"
+            };
+            
+            return View();
+            //return View("HtmlHelpers"); istedıgın view a gitmesi için
+            //return View("HtmlHelpers", usernew); istedıgın view a gitmesi için
+            //return RedirectToAction("HtmlHelpers", "User"); baska adrese yonlendırme dırek oraya
+
+        }
+    }
+}
---
+++ HtmlAndTagHelper/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace HtmlAndTagHelper.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
