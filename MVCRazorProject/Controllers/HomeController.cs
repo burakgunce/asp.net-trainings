9bf0ae2fb6399a2051d6ba39f96e88d1c7477d50
burakgunce <brk.gnc9@hotmail.com>
2023-12-21 09:33:56 +0300

    .

---
+++ MVCRazorProject/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCRazorProject.Models;
+using System.Diagnostics;
+
+namespace MVCRazorProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Ornek1()
+        {
+            User user = new User();
+            user.Ad = "burak";
+            user.Soyad = "gunce";
+            user.KullaniciAdi = "bg61";
+            return View(user);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCRazorProject/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCRazorProject.Models
+{
+    public class User
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string KullaniciAdi { get; set; }
+    }
+}
