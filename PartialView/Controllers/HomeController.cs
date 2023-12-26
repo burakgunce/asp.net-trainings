0380d5135b3c275c7795932c159cd6161175c2c6
burakgunce <brk.gnc9@hotmail.com>
2023-12-26 10:06:49 +0300

    .

---
+++ PartialView/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialView.Models;
+using System.Diagnostics;
+
+namespace PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { ProductId = 1, ProductName = "Product 1", 
+                    CategoryName = "Category 1", Description = "Description 1", Price = 10.50M},
+                new Product { ProductId = 2, ProductName = "Product 1", 
+                    CategoryName = "Category 2", Description = "Description 2", Price = 10.50M},
+                new Product { ProductId = 3, ProductName = "Product 1", 
+                    CategoryName = "Category 2", Description = "Description 3", Price = 10.50M},
+                new Product { ProductId = 4, ProductName = "Product 1", 
+                    CategoryName = "Category 2", Description = "Description 4", Price = 10.50M}
+
+            };
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PartialView/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialView.Models;
+
+namespace PartialView.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { ProductId = 1, ProductName = "Product 1",
+                    CategoryName = "Category 1", Description = "Description 1", Price = 10.50M},
+                new Product { ProductId = 2, ProductName = "Product 1",
+                    CategoryName = "Category 2", Description = "Description 2", Price = 10.50M},
+                new Product { ProductId = 3, ProductName = "Product 1",
+                    CategoryName = "Category 2", Description = "Description 3", Price = 10.50M},
+                new Product { ProductId = 4, ProductName = "Product 1",
+                    CategoryName = "Category 2", Description = "Description 4", Price = 10.50M}
+
+            };
+            return View();
+        }
+    }
+}
