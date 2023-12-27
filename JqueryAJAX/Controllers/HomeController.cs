c2d8fdc5ad07bf3321754613f414cbb228d6b983
burakgunce <brk.gnc9@hotmail.com>
2023-12-27 12:28:41 +0300

    .

---
+++ JqueryAJAX/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using JqueryAJAX.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JqueryAJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public int Add(int number1, int number2)
+        {
+            int total = number1 + number2;
+            return total;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
